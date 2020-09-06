c47a2048fd818743379618ca3cc96be7ecb615c0
ferret-san <diego.ferrer@ufl.edu>
2020-09-06 19:15:05 -0400

    Spelling errors

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-ee7a44530c01553b7514.js"
+    "url": "webpack-runtime-f0dea1cd0aa39dc11083.js"
   },
   {
     "url": "framework-fd2b85530eec85104a39.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "7a0bd3bf97fef71edeef4685abb31017"
+    "revision": "f7e8f3f062ed250d159ae3db05d441eb"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-0097d26fbd474b34ff9b.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "10b538a88746f508ff6e8c888b88e800"
+    "revision": "c286fc9c11f1167175d2981e85eded79"
   },
   {
     "url": "polyfill-5ace7e86a4d67524f45c.js"
